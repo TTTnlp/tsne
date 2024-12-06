a54fe0c177e5a88f05173d22977be037c2235764
Yu Tian <58928581+TTTnlp@users.noreply.github.com>
2024-12-06 16:51:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -8,8 +8,8 @@ torch.manual_seed(1)
 feature = torch.rand(2000, 256)
 torch.save(feature, 'feature.pt')
 
-domain_1 = torch.zeros(100, dtype=torch.long)
-domain_2 = torch.ones(100, dtype=torch.long)
+domain_1 = torch.zeros(1000, dtype=torch.long)
+domain_2 = torch.ones(1000, dtype=torch.long)
 domain_label = torch.cat((domain_1, domain_2))
 torch.save(domain_label, 'domain_label.pt')
 
@@ -17,6 +17,23 @@ emotion_1 = torch.randint(0,7,(1000,))
 emotion_2 = torch.randint(7,14,(1000,))
 emotion_label = torch.cat((emotion_1, emotion_2))
 torch.save(emotion_label, 'emotion_label.pt')
+
+class Domain_classifier(nn.Module):
+    def __init__(self, input_dim, output_dim) -> None:
+        super().__init__()
+        
+        self.fc = nn.Linear(input_dim, output_dim)
+        
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self,x,y):
+        
+        x = self.fc(x)  # 通过第一个全连接层
+
+        loss = self.criterion(x,y)
+        
+        return loss
+    
 
 class M(nn.Module):
     def __init__(self, *args, **kwargs) -> None:
@@ -27,17 +44,23 @@ class M(nn.Module):
         self.fc2 = nn.Linear(256,256)
 
         self.c = nn.Linear(256, 14)
+        self.domain_c = Domain_classifier(256, 2)
 
         self.criterion = nn.CrossEntropyLoss()
 
-    def forward(self,x,y):
+    def forward(self,x,y,domain_label):
         x = self.fc1(x)  # 通过第一个全连接层
         x = self.relu(x)  # 激活函数
         output = self.fc2(x)  # 通过第二个全连接层
 
         x = self.c(output)
-
+        
         loss = self.criterion(x,y)
+        
+        domain_loss = self.domain_c(output,domain_label)
+        
+        loss += domain_loss
+
         y = F.softmax(x, dim=-1)
         y = torch.argmax(y, dim=-1)
         return output,loss,y
@@ -58,10 +81,24 @@ new_label = new_label.to('cuda')
 model = M().to('cuda')
 
 
+domain_label = torch.load('domain_label.pt')
+
+new_domain_label = []
+for l in domain_label:
+    temp_label = [0] * 2
+    temp_label[l] = 1
+    new_domain_label.append(temp_label)
+new_domain_label = torch.tensor(new_domain_label,dtype=torch.float32)
+new_domain_label = new_domain_label.to('cuda')
+
+
+model = M().to('cuda')
+
+
 optimizer = optim.Adam(model.parameters(),lr=1e-3)
 for e in range(200):
     model.train()
-    _,loss,y = model(feature,new_label)
+    _,loss,y = model(feature,new_label,new_domain_label)
     print(f'第{e}个epoch的loss：为{loss}')
     optimizer.zero_grad()
     loss.backward()
@@ -71,13 +108,11 @@ for e in range(200):
 torch.save(model.state_dict(), 'model.pth')
 print("Model saved successfully!")
 
-
-
 model.load_state_dict(torch.load('model.pth'))
 model.eval()  # 切换到评估模式
 with torch.no_grad():  # 禁用梯度计算
     # 假设我们使用相同的输入特征来做推理
-    output, _, predicted_labels = model(feature, new_label)
+    output, _, predicted_labels = model(feature, new_label, new_domain_label)
     pred = predicted_labels.tolist()
     torch.save(output, 'output.pt')
 correct = 0
